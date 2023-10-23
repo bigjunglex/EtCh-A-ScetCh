97c36b1655ef998d1f5d1105e99e5059fb664628
bigjunglex <genasamorez@yandex.ru>
2023-10-23 19:54:19 +0300

    rgb randomize added

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const field = document.getElementById("ground");
 const size = document.getElementById("size");
-const gridMaker = function(side) {
+const color = document.getElementById("rgb");
+const noColor = document.getElementById("black");
+const colorState = document.getElementById("state");
+let rgbMode = false;
+const gridMaker = function(side, color) {
     let containerSize = 480 * 480;
     let sideSize = Math.sqrt(containerSize / (side ** 2));
     
@@ -14,36 +18,36 @@ const gridMaker = function(side) {
         sektor.style.height = `${sideSize - 2}px`;
         sektor.style.width = `${sideSize - 2}px`;
         sektor.style.border = "solid 1px";
-        sektor.addEventListener("mouseover", (event) => {
-            event.target.style.background = "black";
-        });
+        if (!color){
+            sektor.addEventListener("mouseover", (event) => {
+                event.target.style.background = "black";
+            });
+        } else {
+            let rgbRandom = Math.floor(Math.random()*16777215).toString(16);
+            sektor.addEventListener("mouseover", (event) => {
+                event.target.style.background = "#" + rgbRandom;
+            });
+        }
         field.appendChild(sektor)
-    }
+    };
 
-}
+};
+
+color.addEventListener("click", () => {
+    rgbMode = true;
+    colorState.textContent = "RGB"
+});
+noColor.addEventListener("click", () => {
+    rgbMode = false;
+    colorState.textContent = "Black";
+});
+
 
 size.addEventListener("click", function() {
     let gridSide = prompt("Enter number of squares per side");
     if (gridSide > 64) {
         alert("Number of squares per side should not be over 64");
     } else {
-        gridMaker(gridSide);
+        gridMaker(gridSide, rgbMode);
     }
 });
-
-// grid 16x16
-// for (let i = 0; i < 256; i++) {
-//     let sektor = document.createElement("div");
-//     sektor.style.background = "white"
-//     sektor.style.height = "28px";
-//     sektor.style.width = "28px";
-//     sektor.style.border = "solid 1px";
-//     sektor.addEventListener("mouseover", (event) => {
-//         event.target.style.background = "black";
-//     });
-//     field.appendChild(sektor)
-// };
-
-
-
-// sektor.classList.add('sektor');
